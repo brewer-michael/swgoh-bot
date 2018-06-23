917275d0fdadbbfbd5b2778890f1c342b940d385
brewer-michael <mb1985@gmail.com>
2018-06-23 10:27:08 -0400

    Building out structure for the bot

--- slj-bot/secret.js
+++
@@ -1,1 +0,0 @@
-export const botToken = 'MzYwODAxNzI4NjQ5NzU2Njcz.DKa2Vw.vHYwtJG5fF_pDXQppUt_dJoSyD4'
---
+++ slj-bot/src/secret.js
@@ -0,0 +1,2 @@
+#sljbot token
+export const botToken = 'NDU5NzQzMzcwNTAwMjQzNDc3.Dg_SfQ.uF6U5SBE6x1rj0Zr95vxzIymowU'
